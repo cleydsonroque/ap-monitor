08b2b499627a8aa00a29a964d72ccc570ccd7d50
B4RW <B4RW@petrobras.com.br>
2023-10-26 01:58:56 -0300

    todos as extrações implementadas

--- src/models/scraping/scr-5andar.py
+++ src/models/scraping/scr-5andar.py
@@ -8,6 +8,20 @@ URL = f'https://www.quintoandar.com.br/alugar/imovel/{BAIRRO}-niteroi-rj-brasil/
 html = requests.get(URL).text
 sel = Selector(text=html)
 
+# Retorna True quando não encontra imóveis
+TEXTO_CLASS_H4_N_ENCONTRADO = 'CozyTypography'
+html_h4 = sel.css(f'h4[class*={TEXTO_CLASS_H4_N_ENCONTRADO}]')
+alerta = html_h4.css('span::text').get()
+if 'encontramos' in alerta:
+    print(alerta)
+
+# Retorna a quantidade de imóveis encontrados
+TEXTO_CLASS_P_QTD_ENCONTRADA = 'CozyTypography'
+html_p = sel.css(f'p[class*={TEXTO_CLASS_P_QTD_ENCONTRADA}]')
+quantidade = html_p.css('span::text').get()
+if not 'alerta' in quantidade:
+    print(quantidade)
+
 # Retorna uma lista com a url do anuncio do imóvel.   
 TEXTO_COMUM_URL_ANUNCIO = f'apartamento-'
 url_anuncio = sel.css(f'a[href*={TEXTO_COMUM_URL_ANUNCIO}]::attr(href)').getall()
@@ -33,6 +47,5 @@ if len(info_anuncio)/len(url_anuncio) == QTD_ELEMENTOS:
         imovel[7] = imovel[7][imovel[7].find('R')+3:].replace('.','')
         imovel[8] = imovel[8][imovel[8].find('R')+3:].replace('.','')
     print(dados)
-    print(len(dados))
 else:
     print('Rever a lógica que lista as informações do anuncio (linhas 17 a 19)')
