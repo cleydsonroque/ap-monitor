fc52822e63b13b3d397669bd282223562a5ce7e1
B4RW <B4RW@petrobras.com.br>
2023-11-06 21:26:50 -0300

    teste db

--- src/models/scr_orm.py
+++ src/models/scr_orm.py
@@ -10,7 +10,7 @@ class DataB():
     '''Implementa ORM parametrizado para manipular o BD das informações obtidas a partir do scraping dos anuncios dos imóveis.'''       
 
     def criar_conexão(self):
-        self.con = sqlite3.connect(BD)
+        self.con = sqlite3.connect('models/ap.sqlite3')
         self.cur = self.con.cursor()
     
     def encerrar_conexao(self):
