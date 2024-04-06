4dd5e29e2bb938b5462abc78a230a80bed4b6201
B4RW <B4RW@petrobras.com.br>
2024-04-06 16:17:03 -0300

    atualizado váriavel de ambiente

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
+import os
 from models.scraping.scr_5andar import Scr5Andar
 from models.scr_orm import DataB
 from models.send_ap import to_send
-from models.token import TOKEN, CHAT_ID
 
+load_dotenv()
+token = os.environ.get('TELEGRAM_API_KEY')
+chat_id = os.environ.get('CHAT_ID')
 
 bairros = ['piratininga', 'barreto', 'fonseca', 'icarai', 'centro']
 for bairro in bairros:
@@ -10,4 +14,4 @@ for bairro in bairros:
     bd = DataB()
     bd.insere_dados(bairro=bairro, dados=lista)
     anuncios = bd.anunciado_hoje(bairro=bairro)
-    to_send(anuncios, TOKEN, CHAT_ID)
+    to_send(anuncios, token, chat_id)
