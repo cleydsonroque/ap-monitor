40012d26a118ecaa5e83c9318dd0415d18ad1ce1
B4RW <B4RW@petrobras.com.br>
2024-04-08 23:41:31 -0300

    atualização DB

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ from models.send_ap import to_send
 load_dotenv()
 token = os.environ.get('TELEGRAM_API_KEY')
 chat_id = os.environ.get('CHAT_ID')
-print(token)
-print(chat_id)
+
 
 # bairros = ['piratininga', 'barreto', 'fonseca', 'icarai', 'centro']
-bairros = ['piratininga', 'barreto']
-for bairro in bairros:
-    lista = Scr5Andar(bairro=bairro).saida()
-    bd = DataB()
-    bd.insere_dados(bairro=bairro, dados=lista)
-    anuncios = bd.anunciado_hoje(bairro=bairro)
-    to_send(anuncios, token, chat_id)
+# bairros = ['piratininga', 'barreto']
+# for bairro in bairros:
+#     lista = Scr5Andar(bairro=bairro).saida()
+#     bd = DataB()
+#     bd.insere_dados(bairro=bairro, dados=lista)
+#     anuncios = bd.anunciado_hoje(bairro=bairro)
+#     to_send(anuncios, token, chat_id)
 print('finalizado')
